d92eda48da4cfa94f46ee6fa7261a0480b961246
sebastianalexisft@gmail.com <s.fernndeztoro@uandresbello.edu>
2023-06-29 21:05:48 -0400

    Mención autores en Servlets

--- Grupal1/src/main/java/servlets/CrearCapacitacionServlet.java
+++ Grupal1/src/main/java/servlets/CrearCapacitacionServlet.java
@@ -10,7 +10,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.DatosCapacitacion;
-
+/**
+ * 
+ * @author Grupo 5: Sabina Leal, Juan Barrientos, Manuel Chavez, Sebastian
+ *         Fernandez, Cinthya Caldera.
+ *
+ */
 /**
  * Servlet implementation class CrearCapacitacion
  */
--- Grupal1/src/main/java/servlets/InicioServlet.java
+++ Grupal1/src/main/java/servlets/InicioServlet.java
@@ -7,7 +7,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+/**
+ * 
+ * @author Grupo 5: Sabina Leal, Juan Barrientos, Manuel Chavez, Sebastian
+ *         Fernandez, Cinthya Caldera.
+ *
+ */
 /**
  * Servlet implementation class Inicio
  */
